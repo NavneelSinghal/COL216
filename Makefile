d2675ea67f48d507e9cbde50d9830cdf913a58b9
Navneel Singhal <navneel.singhal@ymail.com>
2020-08-09 13:55:33 +0530

    testing

---
+++ Makefile
@@ -0,0 +1,4 @@
+all:
+	g++ -o a8 ./Assignment8/simul.cpp
+	g++ -o a9 ./Assignment9/simul.cpp
+	g++ -o a10 ./Assignment10/new_simult19.cpp
--- compare.sh
+++ compare.sh
@@ -1,10 +1,13 @@
-g++ -o a8 ./Assignment8/simul.cpp
-g++ -o a9 ./Assignment9/simul.cpp
-g++ -o a10 ./Assignment10/new_simult19.cpp
+./a8 Assignment9/fact_registers.txt < Assignment9/fact.txt 2> status8 > /dev/null
+./a9 Assignment9/fact_registers.txt < Assignment9/fact.txt 2> status9 > /dev/null
 
-#./a8 inp_registers < inp > /dev/null 2> status8
-#./a9 inp_registers < inp > /dev/null 2> status9
-#./a10 0.5 2 < inp > /dev/null 2> status10
+echo "checking 8 and 9"
+diff status8 status9
+echo "checking 8 and 10"
+diff status8 status10
+
+./a8 Assignment8/fact_registers < Assignment8/fact.txt 2> ./status8 > /dev/null
+./a9 Assignment8/fact_registers < Assignment8/fact.txt 2> ./status9 > /dev/null
 
 echo "checking 8 and 9"
 diff status8 status9
