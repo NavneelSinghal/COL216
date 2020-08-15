57dd232c2848968b55a963ead0ab4264e56806b3
Navneel Singhal <navneel.singhal@ymail.com>
2020-08-15 23:04:50 +0530

    correct plot

--- Assignment10/plot_for_a10.sh
+++ Assignment10/plot_for_a10.sh
@@ -6,11 +6,12 @@ for ((i=1;i<=9;i=$(($i + 1)))); do
     ./a10 tests/oneton_reg.txt 0.$i 2 < tests/oneton.txt > /dev/null 2>/dev/null
 done
 
+gnuplot plotscript.gp
+
 rm -f out_file_N
 
 for ((i=1;i<=9;i=$(($i + 1)))); do
     ./a10 tests/oneton_reg.txt 0.7 $i < tests/oneton.txt > /dev/null 2>/dev/null
 done
 
-gnuplot plotscript.gp
 gnuplot plotscript2.gp
